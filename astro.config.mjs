44f20770584317701fa0c1a460a413969b1b04fe
Zac Lucarelli <zac.lucarelli@outlook.com>
2022-11-28 00:19:01 +1100

    Finish 3D head + interaction, remove LA for package

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,5 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({config: { applyBaseStyles: true }}), image(), mdx(), prefetch()]
+  integrations: [react(), tailwind({config: { applyBaseStyles: true }}), image({serviceEntryPoint: '@astrojs/image/sharp'}), mdx(), prefetch()]
 });
--- src/components/Junk.tsx
+++ src/components/Junk.tsx
@@ -1,49 +1,119 @@
-import { Suspense, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas, useLoader } from '@react-three/fiber'
 import { Bounds, useBounds, OrbitControls, ContactShadows, useGLTF, PresentationControls, useCursor } from '@react-three/drei'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { MeshStandardMaterial } from 'three'
 
 
 const items = {
-  "VR_Headset": {
+  "VR": {
     title: "VR",
     description: "I love VR! I was an early adopter and have been following the scene closely after getting my vive in 2016. I have a silly amount of hours in beat saber"
-  }
-}
+  },
+  "Camera_mesh": {
+    title: 'Photography',
+    description: 'Taking photos is a fun hobby, although I usually end up just taking photos of the dog. I shoot on a Fujifilm XT-2 with a 16-55mm f/2.8 lens'
+  },
+  "Headphones": {
+    title: 'Music',
+    description: "I love music, especially when it's live. I've got a decent little collection of records too. Some of my favorite artists are The Smith Street Band, Kero kero bonito, Jeff Rosenstock and Anamanaguchi",
+  },
+  "Coffee": {
+    title: 'Coffee',
+    description: "i love me some bean juice, just inject it straight into my veins",
+  },
+  "Duck": {
+    title: 'quack quack',
+    description: '-duck'
+  },
+  "Pizza_Slice": {
+    title: 'pizza pizza',
+    description: '-pizza'
+  },
+  "Tent": {
+    title: 'Camping',
+    description: "I love camping and was big on the outdoors club in uni. I was even on the committee for a bit. I've also been climbing for about 5 years now and love a good climbing trip.",
+  },
+  "Pan": {
+    title: 'Cooking',
+    description: "I'm not half bad at cooking. I'll make you the best burrito you've had in your life."
+  },
+  "Houseplant": {
+    title: 'House plants',
+    description: "Catch me at bunnings on the weekend buying more plants than I can handle. I also love gardening!"
+  },
+  "Curly": {
+    title: "I'm just a decorative rope",
+    description: "leave me alone",
+  },
+  // "Zac_1": {
+  //   title: "That's my beard",
+  //   description: "it's itchy :(",
+  // },
+  // "Zac_2": {
+  //   title: "2",
+  //   description: "2",
+  // },
+  // "Zac_3": {
+  //   title: "Got ya nose",
+  //   description: "",
+  // },
+  // "Zac_4": {
+  //   title: "4",
+  //   description: "2",
+  // },
 
-/*
-Camera - taking photos is a fun hobby of mine, although I usually end up just taking photos of the dog
-Headphones - I love live music and listening to music, some of my favorite artists are The Smith Street Band, Kero kero bonito, Jeff Rosenstock and Anamanaguchi
-Coffee - i love me some bean juice, just inject it straight into my veins
-Camping shit - I'm really into bouldering and climbing, and I love camping and hiking
-Duck - "quack quack" - duck
-Pizza - who doesn't love a good .pizza domain to host all your websites on
-Pan - I'm not half bad at cooking. I'll make you the best burrito you've had in your life.
-House plant - Catch me at bunnings on the weekend buying too many plants. I also love gardening!
-*/
+}
 
 function setText(name) {
   document.getElementById("text").innerHTML = items[name].title;
   document.getElementById("desc").innerHTML = items[name].description;
 }
 
+const nodes = [
+  "Curly",
+  "Camera_mesh",
+  "Duck",
+  "VR",
+  "Coffee",
+  "Houseplant",
+  "Pan",
+  "Tent",
+  "Headphones",
+  "Pizza_Slice",
+  "Sphere",
+  "Zac_1",
+  "Zac_2",
+  "Zac_3",
+  "Zac_4",
+  "Zac_5",
+]
+
 export default function Junk(props) {
+
+  // const [material, setMaterial] = useState(null);
+  // useEffect(() => {
+
+  //   //load texture
+  //   const map = await useLoader(TextureLoader, '/Bake.webp');
+  //   setMaterial(new MeshStandardMaterial({ map: null }));
+  // }, []);
+
+
   return (
     <div className='relative h-full'>
       <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]} className={''}>
         <spotLight position={[-100, -100, -100]} intensity={0.2} angle={0.3} penumbra={1} />
+        {/* <spotLight position={[-100, -100, -100]} intensity={4} angle={0.3} penumbra={1} /> */}
         <hemisphereLight color="white" groundColor="#ff0f00" position={[-7, 25, 13]} intensity={1} />
         <Suspense fallback={null}>
           <Bounds fit clip observe margin={1.2}>
             <SelectToZoom >
-              <Model name="Curly" position={[1, -11, -20]} rotation={[2, 0, -0]} />
-              <Model name="DNA" position={[20, 0, -17]} rotation={[1, 1, -2]} />
-              <Model name="Headphones" position={[20, 2, 4]} rotation={[1, 0, -1]} />
-              <Model name="Notebook" position={[-21, -15, -13]} rotation={[2, 0, 1]} />
-              <Model name="Rocket003" position={[18, 15, -25]} rotation={[1, 1, 0]} />
-              <Model name="Roundcube001" position={[-25, -4, 5]} rotation={[1, 0, 0]} scale={0.5} />
-              <Model name="Table" position={[1, -4, -28]} rotation={[1, 0, -1]} scale={0.5} />
-              <Model name="VR_Headset" position={[7, -15, 28]} rotation={[1, 0, -1]} scale={5} />
-              <Model name="Zeppelin" position={[-20, 10, 10]} rotation={[3, -1, 3]} scale={0.005} />
+              {
+                nodes.map((node) => {
+                  return <Model key={node} name={node} />
+                })
+              }
             </SelectToZoom>
           </Bounds>
           {/* <ContactShadows rotation-x={Math.PI / 2} position={[0, -35, 0]} opacity={0.2} width={200} height={200} blur={1} far={50} /> */}
@@ -60,22 +130,29 @@ export default function Junk(props) {
   )
 }
 
-function Model({ name, ...props }) {
-  const { nodes } = useGLTF('/junk.glb');
+function Model({ name, material, ...props }) {
+  const { nodes, materials } = useGLTF('/junk_test.gltf');
 
   const [hovered, setHovered] = useState(false)
   useCursor(hovered);
 
+  if (!nodes[name]) return null;
+
+  console.log(nodes[name].material);
+
   return <mesh
     geometry={nodes[name].geometry}
     material={nodes[name].material}
-    name={name} material-emissive="#4E1CBE"
+    name={name}
+    material-emissive={name.includes("Zac") ? "#000000" : "#4E1CBE"}
+    material-side={name === "Houseplant" ? 2 : 2}
     material-roughness={1}
     onClick={() => setText(name)}
-    onPointerOver={() => setHovered(true)} 
+    onPointerOver={() => setHovered(true)}
     onPointerOut={() => setHovered(false)}
     {...props}
-    dispose={null} />
+    dispose={null} >
+  </mesh>
 }
 
 // This component wraps children in a group with a click handler
